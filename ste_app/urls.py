164249d8fdbf2f361341814bdbc9e8c3eef64c2a
Talha Khubaib <talhadogar581@gmail.com>
2024-04-07 00:20:27 +0500

    Cleaned Urls.py

--- ste_app/urls.py
+++ ste_app/urls.py
@@ -6,7 +6,6 @@ from .import HodViews, StaffViews, StudentViews
 
 urlpatterns = [
     path('', views.loginPage, name="login"),
-    # path('accounts/', include('django.contrib.auth.urls')),
     path('doLogin/', views.doLogin, name="doLogin"),
     path('get_user_details/', views.get_user_details, name="get_user_details"),
     path('logout_user/', views.logout_user, name="logout_user"),
