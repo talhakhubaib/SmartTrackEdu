28a4f9391ce0a3aaab321f8e03d733c6e69f84df
Talha Khubaib <talhadogar581@gmail.com>
2024-04-07 02:23:48 +0500

    Register models with Django admin.

--- ste_app/admin.py
+++ ste_app/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, AdminHOD, Staffs, Courses, Subjects, Students, Attendance, AttendanceReport, LeaveReportStudent, LeaveReportStaff, FeedBackStudent, FeedBackStaffs, NotificationStudent, NotificationStaffs
-
-# Register your models here.
 class UserModel(UserAdmin):
     pass
 
