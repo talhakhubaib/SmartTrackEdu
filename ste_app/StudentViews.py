ba18049c636ae4c3b718515a4ed2f9cc6b740819
Talha Khubaib <talhadogar581@gmail.com>
2024-04-07 00:24:08 +0500

    Cleaned StudentViews.py

--- ste_app/StudentViews.py
+++ ste_app/StudentViews.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
-from django.core.files.storage import FileSystemStorage #To upload Profile Picture
+from django.core.files.storage import FileSystemStorage
 from django.urls import reverse
-import datetime # To Parse input DateTime into Python Date Time Object
+import datetime
 
 from ste_app.models import CustomUser, Staffs, Courses, Subjects, Students, Attendance, AttendanceReport, LeaveReportStudent, FeedBackStudent, StudentResult
 
@@ -44,13 +44,11 @@ def student_home(request):
 def student_view_attendance(request):
     student = Students.objects.get(admin=request.user.id) # Getting Logged in Student Data
     course = student.course_id # Getting Course Enrolled of LoggedIn Student
-    # course = Courses.objects.get(id=student.course_id.id) # Getting Course Enrolled of LoggedIn Student
     subjects = Subjects.objects.filter(course_id=course) # Getting the Subjects of Course Enrolled
     context = {
         "subjects": subjects
     }
     return render(request, "student_template/student_view_attendance.html", context)
-
 
 def student_view_attendance_post(request):
     if request.method != "POST":
@@ -62,26 +60,15 @@ def student_view_attendance_post(request):
         start_date = request.POST.get('start_date')
         end_date = request.POST.get('end_date')
 
-        # Parsing the date data into Python object
         start_date_parse = datetime.datetime.strptime(start_date, '%Y-%m-%d').date()
         end_date_parse = datetime.datetime.strptime(end_date, '%Y-%m-%d').date()
 
-        # Getting all the Subject Data based on Selected Subject
         subject_obj = Subjects.objects.get(id=subject_id)
-        # Getting Logged In User Data
         user_obj = CustomUser.objects.get(id=request.user.id)
-        # Getting Student Data Based on Logged in Data
         stud_obj = Students.objects.get(admin=user_obj)
 
-        # Now Accessing Attendance Data based on the Range of Date Selected and Subject Selected
         attendance = Attendance.objects.filter(attendance_date__range=(start_date_parse, end_date_parse), subject_id=subject_obj)
-        # Getting Attendance Report based on the attendance details obtained above
         attendance_reports = AttendanceReport.objects.filter(attendance_id__in=attendance, student_id=stud_obj)
-
-        # for attendance_report in attendance_reports:
-        #     print("Date: "+ str(attendance_report.attendance_id.attendance_date), "Status: "+ str(attendance_report.status))
-
-        # messages.success(request, "Attendacne View Success")
 
         context = {
             "subject_obj": subject_obj,
