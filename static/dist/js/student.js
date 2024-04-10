1b40cc37356942dcf1eed61fbccf838820ceceb4
Talha Khubaib <talhadogar581@gmail.com>
2024-04-11 00:28:26 +0500

    added custom js file for date validation

---
+++ static/dist/js/student.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("#fetch_student").click(function(e) {
+        e.preventDefault();
+        var startDate = $("#start_date").val();
+        var endDate = $("#end_date").val();
+        if (startDate === "" || endDate === "") {
+            alert("Please select both start and end dates.");
+        } else {
+            $("#attendanceForm").submit();
+        }
+    });
+});
